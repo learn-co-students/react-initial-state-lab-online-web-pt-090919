9ae290c6be2bc1d10212189c9ab49d69ddd04550
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-16 04:57:18 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,16 @@
 // your Bomb code here!
+import React, { Component} from 'react'
+
+export default class Bomb extends Component {
+  constructor (props){
+    super()
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+  
+   render() {
+    
+  }
+  
+}
