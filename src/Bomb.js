6328264c0b214b34bfa7607e3767d85fc006c17c
kalmegati <kalmegati@gmail.com>
2020-05-29 23:24:24 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+  
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+      
+    trigger = () => {
+        return ((this.state.secondsLeft === 0) ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`)
+    }
+
+  render() {
+    return (
+      <div id="matrix">
+        {this.trigger()}
+      </div>
+    )
+  }
+
+}
